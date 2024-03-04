25f47a77ccecb7a7d9ef7e257a556315388ba210
Brian <5279357+barmst02@users.noreply.github.com>
2024-03-04 14:00:16 -0600

    Working

--- LilyGo/src/main.cpp
+++ LilyGo/src/main.cpp
@@ -1,187 +1,130 @@
 #include <TinyGPSPlus.h>
+
 /*
-   This sample code demonstrates just about every built-in operation of TinyGPSPlus (TinyGPSPlus).
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
+   This sample code demonstrates the normal use of a TinyGPSPlus (TinyGPSPlus) object.
 */
 static const uint32_t GPSBaud = 9600;
 
 // The TinyGPSPlus object
 TinyGPSPlus gps;
 
-
-// For stats that happen every 5 seconds
-unsigned long last = 0UL;
-
 void setup()
 {
   Serial.begin(115200);
   Serial0.begin(GPSBaud);
 
-  Serial.println(F("KitchenSink.ino"));
-  Serial.println(F("Demonstrating nearly every feature of TinyGPSPlus"));
+  Serial.println(F("FullExample.ino"));
+  Serial.println(F("An extensive example of many interesting TinyGPSPlus features"));
   Serial.print(F("Testing TinyGPSPlus library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
   Serial.println(F("by Mikal Hart"));
   Serial.println();
+  Serial.println(F("Sats HDOP  Latitude   Longitude   Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum"));
+  Serial.println(F("           (deg)      (deg)       Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail"));
+  Serial.println(F("----------------------------------------------------------------------------------------------------------------------------------------"));
+}
+
+// This custom version of delay() ensures that the gps object
+// is being "fed".
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (Serial0.available())
+      gps.encode(Serial0.read());
+  } while (millis() - start < ms);
+}
+
+static void printFloat(float val, bool valid, int len, int prec)
+{
+  if (!valid)
+  {
+    while (len-- > 1)
+      Serial.print('*');
+    Serial.print(' ');
+  }
+  else
+  {
+    Serial.print(val, prec);
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len; ++i)
+      Serial.print(' ');
+  }
+  smartDelay(0);
+}
+
+static void printInt(unsigned long val, bool valid, int len)
+{
+  char sz[32] = "*****************";
+  if (valid)
+    sprintf(sz, "%ld", val);
+  sz[len] = 0;
+  for (int i=strlen(sz); i<len; ++i)
+    sz[i] = ' ';
+  if (len > 0) 
+    sz[len-1] = ' ';
+  Serial.print(sz);
+  smartDelay(0);
+}
+
+static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
+{
+  if (!d.isValid())
+  {
+    Serial.print(F("********** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
+    Serial.print(sz);
+  }
+  
+  if (!t.isValid())
+  {
+    Serial.print(F("******** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d:%02d:%02d ", t.hour(), t.minute(), t.second());
+    Serial.print(sz);
+  }
+
+  printInt(d.age(), d.isValid(), 5);
+  smartDelay(0);
+}
+
+static void printStr(const char *str, int len)
+{
+  int slen = strlen(str);
+  for (int i=0; i<len; ++i)
+    Serial.print(i<slen ? str[i] : ' ');
+  smartDelay(0);
 }
 
 void loop()
 {
-  // Dispatch incoming characters
-  while (Serial0.available() > 0)
-    gps.encode(Serial0.read());
+  printInt(gps.satellites.value(), gps.satellites.isValid(), 5);
+  printFloat(gps.hdop.hdop(), gps.hdop.isValid(), 6, 1);
+  printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
+  printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
+  printInt(gps.location.age(), gps.location.isValid(), 5);
+  printDateTime(gps.date, gps.time);
+  printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
+  printFloat(gps.course.deg(), gps.course.isValid(), 7, 2);
+  printFloat(gps.speed.kmph(), gps.speed.isValid(), 6, 2);
+  printStr(gps.course.isValid() ? TinyGPSPlus::cardinal(gps.course.deg()) : "*** ", 6);
 
-  if (gps.location.isUpdated())
-  {
-    Serial.print(F("LOCATION   Fix Age="));
-    Serial.print(gps.location.age());
-    Serial.print(F("ms Raw Lat="));
-    Serial.print(gps.location.rawLat().negative ? "-" : "+");
-    Serial.print(gps.location.rawLat().deg);
-    Serial.print("[+");
-    Serial.print(gps.location.rawLat().billionths);
-    Serial.print(F(" billionths],  Raw Long="));
-    Serial.print(gps.location.rawLng().negative ? "-" : "+");
-    Serial.print(gps.location.rawLng().deg);
-    Serial.print("[+");
-    Serial.print(gps.location.rawLng().billionths);
-    Serial.print(F(" billionths],  Lat="));
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(" Long="));
-    Serial.println(gps.location.lng(), 6);
-  }
+  printInt(gps.charsProcessed(), true, 6);
+  printInt(gps.sentencesWithFix(), true, 10);
+  printInt(gps.failedChecksum(), true, 9);
+  Serial.println();
+  
+  smartDelay(1000);
 
-  else if (gps.date.isUpdated())
-  {
-    Serial.print(F("DATE       Fix Age="));
-    Serial.print(gps.date.age());
-    Serial.print(F("ms Raw="));
-    Serial.print(gps.date.value());
-    Serial.print(F(" Year="));
-    Serial.print(gps.date.year());
-    Serial.print(F(" Month="));
-    Serial.print(gps.date.month());
-    Serial.print(F(" Day="));
-    Serial.println(gps.date.day());
-  }
-
-  else if (gps.time.isUpdated())
-  {
-    Serial.print(F("TIME       Fix Age="));
-    Serial.print(gps.time.age());
-    Serial.print(F("ms Raw="));
-    Serial.print(gps.time.value());
-    Serial.print(F(" Hour="));
-    Serial.print(gps.time.hour());
-    Serial.print(F(" Minute="));
-    Serial.print(gps.time.minute());
-    Serial.print(F(" Second="));
-    Serial.print(gps.time.second());
-    Serial.print(F(" Hundredths="));
-    Serial.println(gps.time.centisecond());
-  }
-
-  else if (gps.speed.isUpdated())
-  {
-    Serial.print(F("SPEED      Fix Age="));
-    Serial.print(gps.speed.age());
-    Serial.print(F("ms Raw="));
-    Serial.print(gps.speed.value());
-    Serial.print(F(" Knots="));
-    Serial.print(gps.speed.knots());
-    Serial.print(F(" MPH="));
-    Serial.print(gps.speed.mph());
-    Serial.print(F(" m/s="));
-    Serial.print(gps.speed.mps());
-    Serial.print(F(" km/h="));
-    Serial.println(gps.speed.kmph());
-  }
-
-  else if (gps.course.isUpdated())
-  {
-    Serial.print(F("COURSE     Fix Age="));
-    Serial.print(gps.course.age());
-    Serial.print(F("ms Raw="));
-    Serial.print(gps.course.value());
-    Serial.print(F(" Deg="));
-    Serial.println(gps.course.deg());
-  }
-
-  else if (gps.altitude.isUpdated())
-  {
-    Serial.print(F("ALTITUDE   Fix Age="));
-    Serial.print(gps.altitude.age());
-    Serial.print(F("ms Raw="));
-    Serial.print(gps.altitude.value());
-    Serial.print(F(" Meters="));
-    Serial.print(gps.altitude.meters());
-    Serial.print(F(" Miles="));
-    Serial.print(gps.altitude.miles());
-    Serial.print(F(" KM="));
-    Serial.print(gps.altitude.kilometers());
-    Serial.print(F(" Feet="));
-    Serial.println(gps.altitude.feet());
-  }
-
-  else if (gps.satellites.isUpdated())
-  {
-    Serial.print(F("SATELLITES Fix Age="));
-    Serial.print(gps.satellites.age());
-    Serial.print(F("ms Value="));
-    Serial.println(gps.satellites.value());
-  }
-
-  else if (gps.hdop.isUpdated())
-  {
-    Serial.print(F("HDOP       Fix Age="));
-    Serial.print(gps.hdop.age());
-    Serial.print(F("ms raw="));
-    Serial.print(gps.hdop.value());
-    Serial.print(F(" hdop="));
-    Serial.println(gps.hdop.hdop());
-  }
-
-  else if (millis() - last > 5000)
-  {
-    Serial.println();
-    if (gps.location.isValid())
-    {
-      static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
-      double distanceToLondon =
-        TinyGPSPlus::distanceBetween(
-          gps.location.lat(),
-          gps.location.lng(),
-          LONDON_LAT, 
-          LONDON_LON);
-      double courseToLondon =
-        TinyGPSPlus::courseTo(
-          gps.location.lat(),
-          gps.location.lng(),
-          LONDON_LAT, 
-          LONDON_LON);
-
-      Serial.print(F("LONDON     Distance="));
-      Serial.print(distanceToLondon/1000, 6);
-      Serial.print(F(" km Course-to="));
-      Serial.print(courseToLondon, 6);
-      Serial.print(F(" degrees ["));
-      Serial.print(TinyGPSPlus::cardinal(courseToLondon));
-      Serial.println(F("]"));
-    }
-
-    Serial.print(F("DIAGS      Chars="));
-    Serial.print(gps.charsProcessed());
-    Serial.print(F(" Sentences-with-Fix="));
-    Serial.print(gps.sentencesWithFix());
-    Serial.print(F(" Failed-checksum="));
-    Serial.print(gps.failedChecksum());
-    Serial.print(F(" Passed-checksum="));
-    Serial.println(gps.passedChecksum());
-
-    if (gps.charsProcessed() < 10)
-      Serial.println(F("WARNING: No GPS data.  Check wiring."));
-
-    last = millis();
-    Serial.println();
-  }
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
 }
