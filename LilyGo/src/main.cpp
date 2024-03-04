064cdb58cca6805dad2076c9735bd45b69e398e9
Brian <5279357+barmst02@users.noreply.github.com>
2024-03-04 09:16:33 -0600

    Read from GPS

--- LilyGo/src/main.cpp
+++ LilyGo/src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include "pin_config.h"
 
-// put function declarations here:
-int myFunction(int, int);
+
+//GPS Baud rate
+#define GPSBaud 9600 
+ 
+//Serial Monitor Baud Rate
+#define Serial_Monitor_Baud 115200 
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial0.begin(GPSBaud ); // Initialize serial communication
+  
+  Serial.begin(Serial_Monitor_Baud); // Initialize serial communication
+  Serial.println("Hello T-Display-S3");
+  
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (Serial0.available() > 0)
+    Serial.write(Serial0.read());
+  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
